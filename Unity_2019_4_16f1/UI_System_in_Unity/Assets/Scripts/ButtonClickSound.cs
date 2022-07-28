c2960aff655c8d3e94389c2bbb9e3ffdd44effd7
Gulraiz <gulraiznoorbari@gmail.com>
2022-07-29 01:03:44 +0500

    📦 NEW: Add Unity UI System

---
+++ Unity_2019_4_16f1/UI_System_in_Unity/Assets/Scripts/ButtonClickSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class ButtonClickSound : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    [SerializeField] private AudioSource _audio;
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(onClick);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(onClick);
+    }
+
+    private void onClick()
+    {
+        Debug.Log("PLay Click Sound.");
+        _audio.Play();
+    }
+}
