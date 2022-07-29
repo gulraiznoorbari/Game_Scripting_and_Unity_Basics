086e619f5e4f4f89ecdc8ac9fe3f49bfe33e8941
Gulraiz <gulraiznoorbari@gmail.com>
2022-07-30 00:41:49 +0500

    📦 NEW: Camera movement with player

---
+++ Unity_2019_4_16f1/Camera_Movement/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float _smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float _smoothTime = 0.3f;
+
+    // LateUpdate is called after Update each frame
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        // Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+        // you can use Vector3.SmoothDamp() for dealing with jitters during motion.
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, _smoothTime);
+        transform.position = smoothPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Unity_2019_4_16f1/Camera_Movement/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 2f;
+    private void Update()
+    {   // Transform.translate(new Vector3(x, y, z)) can also be used for movement.
+
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.z < 60) 
+        {
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.z > -85)
+        {
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -82)
+        {
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 90)
+        {
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = new Vector3(53, 4.7f, -30);
+        }
+        
+
+    }
+}
