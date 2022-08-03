9d499375ab640a1d6be69feac9b359161cd0a9de
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-04 00:20:29 +0500

    📦 NEW: Lighting and Menu's in Unity

---
+++ Unity_2019_4_16f1/Start_Menu/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenuUI;
+    private static bool _isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isPaused = false;
+    }
+    
+    public void Pause()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isPaused = true;
+    }
+    
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
---
+++ Unity_2019_4_16f1/Start_Menu/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audio;
+ 
+    private void Start()
+    {
+        _audio.volume = 0;
+        _audio.Play();
+    }
+
+    public void SetVolume(float volume)
+    {
+        _audio.volume = volume;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log(isFullscreen);
+    }
+}
