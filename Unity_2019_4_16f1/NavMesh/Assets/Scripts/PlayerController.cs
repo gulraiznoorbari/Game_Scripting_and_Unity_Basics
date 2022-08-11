08f0995f29438352c92be003d421009427314350
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-12 00:15:38 +0500

    📦 NEW: Add Files

---
+++ Unity_2019_4_16f1/NavMesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private NavMeshAgent _agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
